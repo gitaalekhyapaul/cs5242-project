a1c76b225b2b7a7d167ad3d53f56eed58ad61a6d
plosslaw <plosslaw@arclab.io>
2026-04-26 03:15:48 +0800

    app_metadata

--- main/recommend.py
+++ main/recommend.py
@@ -1,4 +1,11 @@
-# Stage 0: Our Data Catalog
+from models.tisasrec import TiSASRec
+import torch
+from pathlib import Path
+import json
+from pprint import pprint
+
+device = torch.device("cuda" if torch.cuda.is_available() else "mps" if torch.backends.mps.is_available() else "cpu")
+
 CATALOG = {
     "Mobile Apps": {
         "Productivity": ["Notion", "Evernote", "Slack", "Todoist"],
@@ -11,6 +18,8 @@ CATALOG = {
         "Puzzle": ["Portal 2", "Tetris Effect", "Monument Valley", "Candy Crush"]
     }
 }
+
+MAIN_CATEGORIES = ["Mobile Apps", "Games"]
 
 def clear_screen():
     print("\n" * 2)
@@ -35,18 +44,58 @@ def main():
 
     # Stage 1: Initial Category Selection
     print("\nWhat are you interested in today?")
-    main_category = get_choice(list(CATALOG.keys()))
+    main_category = get_choice(MAIN_CATEGORIES)
+
+    #* default to mobilerec settings
+    settings = {
+        "num_items": 10173,
+        "num_categories": 48,
+    }
+    model_checkpoint = Path("metadata") / "mobilerec_model.pt"
+
+    #* switch to steamrec settings
+    if main_category == MAIN_CATEGORIES[1]:
+        settings = {
+            "num_items": 9597,
+            "num_categories": 17,
+        }
+        model_checkpoint = Path("metadata") / "steamrec_model.pt"
+        with open('metadata/game_categories.json', 'r') as file:
+            categories = json.load(file)
+        with open('metadata/game_metadata.json', 'r') as file:
+            metadata = json.load(file)
+    else:
+        with open('metadata/app_categories.json', 'r') as file:
+            categories = json.load(file)
+        with open('metadata/app_metadata.json', 'r') as file:
+            metadata = json.load(file)
+
+    model = TiSASRec(
+        num_items=settings["num_items"],
+        num_categories=settings["num_categories"],
+        num_metadata=5,
+        max_len=50,
+        time_span=256,
+        hidden_size=128,
+        num_blocks=2,
+        num_heads=2,
+        dropout=0.2,
+        device=device,
+    )
+
+    model.load_state_dict(torch.load(model_checkpoint, map_location=device))
 
     while True:
-        # Stage 2: Show Genres based on Stage 1
-        print(f"\n--- {main_category} Genres ---")
-        genres = list(CATALOG[main_category].keys())
-        selected_genre = get_choice(genres)
+        # Stage 2: Show subcategories based on Stage 1
+        print(f"\n--- {main_category} Categories ---")
+        subcategories = list(categories.keys())
+        selected_subcategory = get_choice(subcategories)
 
-        # Stage 3: Show Items in Genre
-        print(f"\n--- Best {selected_genre} {main_category} ---")
-        items = CATALOG[main_category][selected_genre]
-        item_choice = get_choice(items)
+        # Stage 3: Show Items in subcategory
+        print(f"\n--- Category: {selected_subcategory} ({main_category}) ---")
+        item_ids = categories[selected_subcategory]["item_ids"]
+        item_names = [metadata.get(item_id) for item_id in item_ids]
+        item_choice = get_choice(item_names)
 
         if item_choice not in selected_items:
             selected_items.append(item_choice)
@@ -61,6 +110,15 @@ def main():
         if continue_choice == "No, I'm finished":
             break
         clear_screen()
+
+    scores = model.score_all_items(
+        input_ids=input_ids,
+        metadata_seq=metadata_seq,
+        category_seq=category_seq,
+        time_matrix=time_matrix,
+    )
+
+    pprint(scores)
 
     # Stage 6: Final Recommendations Output
     print("\n" + "="*60)
